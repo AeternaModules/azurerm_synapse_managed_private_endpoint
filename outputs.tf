0b5fad9bfe82ad455597676980dcbbbdd494712f
Daniel Allen <dan@aeternagroup.net>
2026-01-11 19:36:26 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "synapse_managed_private_endpoints" {
+  description = "All synapse_managed_private_endpoint resources"
+  value       = azurerm_synapse_managed_private_endpoint.synapse_managed_private_endpoints
+}
+
