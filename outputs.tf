3123cdd9aca4b1930c344040b7e179d832107b36
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:06 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "synapse_managed_private_endpoints" {
-  description = "All synapse_managed_private_endpoint resources"
-  value       = azurerm_synapse_managed_private_endpoint.synapse_managed_private_endpoints
-}
 output "synapse_managed_private_endpoints_fully_qualified_domain_names" {
-  description = "List of fully_qualified_domain_names values across all synapse_managed_private_endpoints"
-  value       = [for k, v in azurerm_synapse_managed_private_endpoint.synapse_managed_private_endpoints : v.fully_qualified_domain_names]
+  description = "Map of fully_qualified_domain_names values across all synapse_managed_private_endpoints, keyed the same as var.synapse_managed_private_endpoints"
+  value       = { for k, v in azurerm_synapse_managed_private_endpoint.synapse_managed_private_endpoints : k => v.fully_qualified_domain_names }
 }
 output "synapse_managed_private_endpoints_name" {
-  description = "List of name values across all synapse_managed_private_endpoints"
-  value       = [for k, v in azurerm_synapse_managed_private_endpoint.synapse_managed_private_endpoints : v.name]
+  description = "Map of name values across all synapse_managed_private_endpoints, keyed the same as var.synapse_managed_private_endpoints"
+  value       = { for k, v in azurerm_synapse_managed_private_endpoint.synapse_managed_private_endpoints : k => v.name }
 }
 output "synapse_managed_private_endpoints_subresource_name" {
-  description = "List of subresource_name values across all synapse_managed_private_endpoints"
-  value       = [for k, v in azurerm_synapse_managed_private_endpoint.synapse_managed_private_endpoints : v.subresource_name]
+  description = "Map of subresource_name values across all synapse_managed_private_endpoints, keyed the same as var.synapse_managed_private_endpoints"
+  value       = { for k, v in azurerm_synapse_managed_private_endpoint.synapse_managed_private_endpoints : k => v.subresource_name }
 }
 output "synapse_managed_private_endpoints_synapse_workspace_id" {
-  description = "List of synapse_workspace_id values across all synapse_managed_private_endpoints"
-  value       = [for k, v in azurerm_synapse_managed_private_endpoint.synapse_managed_private_endpoints : v.synapse_workspace_id]
+  description = "Map of synapse_workspace_id values across all synapse_managed_private_endpoints, keyed the same as var.synapse_managed_private_endpoints"
+  value       = { for k, v in azurerm_synapse_managed_private_endpoint.synapse_managed_private_endpoints : k => v.synapse_workspace_id }
 }
 output "synapse_managed_private_endpoints_target_resource_id" {
-  description = "List of target_resource_id values across all synapse_managed_private_endpoints"
-  value       = [for k, v in azurerm_synapse_managed_private_endpoint.synapse_managed_private_endpoints : v.target_resource_id]
+  description = "Map of target_resource_id values across all synapse_managed_private_endpoints, keyed the same as var.synapse_managed_private_endpoints"
+  value       = { for k, v in azurerm_synapse_managed_private_endpoint.synapse_managed_private_endpoints : k => v.target_resource_id }
 }
 
